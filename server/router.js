ffe3c6daa88a2112485815240af0679e563114d4
testcode369 <purnanandwasave.docs@gmail.com>
2025-05-30 18:27:26 +0530

    Update router.js

--- server/router.js
+++ server/router.js
@@ -25,9 +25,8 @@ export async function handleRequest(request, env, ctx) {
       { ASSET_NAMESPACE: env.__STATIC_CONTENT }
     );
   } catch (err) {
-    const url = new URL(request.url);
-    url.pathname ='./client/index.html';
-   // return new Response('Not Found 2', { status: 404 });
+   
+    return new Response('Not Found 2', { status: 404 });
   }
 
 
