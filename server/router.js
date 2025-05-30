a0079ddf2476b4308df54a97a3004981535783fa
testcode369 <purnanandwasave.docs@gmail.com>
2025-05-30 17:33:35 +0530

    Update router.js

--- server/router.js
+++ server/router.js
@@ -1,4 +1,5 @@
 import { handleJoin, handleAutomatch, handleInvite, handleAcceptInvite } from './matchmaker.js';
+import indexHtml from './client/index.html';
 
 export async function handleRequest(request, env, ctx) {
   const url = new URL(request.url);
@@ -16,6 +17,12 @@ export async function handleRequest(request, env, ctx) {
   if (request.method === 'GET' && pathname === '/api/accept-invite') {
     return handleAcceptInvite(request, env);
   }
+  if (pathname === '/' && request.method === 'GET') {
+    return new Response(indexHtml, {
+      headers: { 'Content-Type': 'text/html' },
+    });
+  }
+
 
   return new Response('Not found', { status: 404 });
 }
