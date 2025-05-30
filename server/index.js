390df355e970a1e3846a373b4ed403255920cc34
testcode369 <purnanandwasave.docs@gmail.com>
2025-05-30 14:48:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,47 @@
+// server/index.js
 import { handleRequest } from './router.js';
 
-export default {
-  async fetch(request, env, ctx) {
-    return handleRequest(request, env, ctx);
+// Add dummy Durable Object classes to satisfy Wrangler
+export class Room {
+  constructor(state, env) {
+    this.state = state;
+    this.env = env;
   }
+
+  async fetch(request) {
+    return new Response('Room Durable Object responding');
+  }
+}
+
+export class Sync {
+  constructor(state, env) {
+    this.state = state;
+    this.env = env;
+  }
+
+  async fetch(request) {
+    return new Response('Sync Durable Object responding');
+  }
+}
+
+export class Spectator {
+  constructor(state, env) {
+    this.state = state;
+    this.env = env;
+  }
+
+  async fetch(request) {
+    return new Response('Spectator Durable Object responding');
+  }
+}
+
+// Default Worker export
+export default {
+  fetch(request, env, ctx) {
+    return handleRequest(request, env, ctx);
+  },
+  // Required Durable Object exports
+  Room,
+  Sync,
+  Spectator
 };
