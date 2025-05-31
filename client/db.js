c986485b8fd155268c935e496059fe969462110e
testcode369 <purnanandwasave.docs@gmail.com>
2025-05-31 19:25:43 +0530

    Delete client directory

--- client/db.js
+++
@@ -1,7 +0,0 @@
-// db.js
-import Dexie from 'https://cdn.jsdelivr.net/npm/dexie@4.0.2/+esm';
-
-export const db = new Dexie('CheckersGame');
-db.version(1).stores({
-  moves: '++id,timestamp,data'
-});
